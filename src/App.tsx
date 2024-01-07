27ea8984bfef8c1e3dfd46c37474508f0f92f1af
Alexis-Lu <alexis.lu.dev@gmail.com>
2024-01-07 15:08:25 +0100

    feat: add exercices functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,167 @@
 import "./App.css";
+import { useState, useEffect, useLayoutEffect } from "react";
+import Event from "./components/event";
+import { Event as EventType } from "./types/event";
 
 function App() {
-  return <div></div>;
+  const windowSizes = useWindowSize();
+  const [eventWithpositions, setEventWithPositions] = useState();
+  const [shouldUpdatePositions, setShouldUpdatePositions] = useState(true);
+  const events: EventType[] = [
+    {
+      id: 1,
+      start: "15:00", // The event starts at 03:00 pm
+      duration: 90, // The duration is expressed in minutes
+    },
+    {
+      id: 2,
+      start: "20:00", // The event starts at 08:00 pm
+      duration: 60, // The duration is expressed in minutes
+    },
+    {
+      id: 3,
+      start: "20:30", // The event starts at 09:00 pm
+      duration: 30, // The duration is expressed in minutes
+    },
+    {
+      id: 4,
+      start: "8:30", // The event starts at 09:00 pm
+      duration: 60, // The duration is expressed in minutes
+    },
+    {
+      id: 5,
+      start: "19:30", // The event starts at 09:00 pm
+      duration: 90, // The duration is expressed in minutes
+    },
+  ];
+
+  const sortAndCleanEvents = (events: EventType[]) => {
+    return events
+      .sort((a, b) => {
+        const startTimeA = getTimeInMinutes(a.start);
+        const startTimeB = getTimeInMinutes(b.start);
+        return startTimeA - startTimeB;
+      })
+      .filter((event) => {
+        const startTime = getTimeInMinutes(event.start);
+        const endTime = startTime + event.duration;
+        return endTime <= 780;
+      });
+  };
+
+  const getTimeInMinutes = (time: string) => {
+    const [hours, minutes] = time.split(":").map(Number);
+    return hours * 60 + minutes;
+  };
+
+  const groupOverlappingEvents = (events: EventType[]) => {
+    const eventsSorted = sortAndCleanEvents(events);
+    let groups = [];
+    let count = 0;
+
+    for (let i = 0; i < events.length; i++) {
+      let currentGroup = [events[i]];
+
+      for (let j = 1; i + j < events.length; j++) {
+        const endTimeCurrent =
+          getTimeInMinutes(currentGroup[currentGroup.length - 1].start) +
+          currentGroup[currentGroup.length - 1].duration;
+        const startTimeNext = getTimeInMinutes(events[i + j].start);
+
+        if (endTimeCurrent > startTimeNext) {
+          count = count + 1;
+          currentGroup.push(events[i + j]);
+        } else {
+          break;
+        }
+      }
+
+      i = i + count;
+
+      groups.push(currentGroup);
+    }
+    return groups;
+  };
+
+  const calculatePositions = async (events: EventType[]) => {
+    let array: any = [];
+    const e = groupOverlappingEvents(events);
+    e.forEach((group) => {
+      group.forEach((element, index) => {
+        const startTime = getTimeInMinutes(element.start) - 480;
+        const top = (startTime / 780) * windowSizes.height;
+        const height = (element.duration / 780) * windowSizes.height;
+        const left = (windowSizes.width / group.length) * index;
+        const width = windowSizes.width / group.length - 10;
+        array.push({
+          ...element,
+          position: {
+            top,
+            height,
+            left,
+            width,
+          },
+        });
+      });
+    });
+    console.log(array);
+    setEventWithPositions(array);
+    setShouldUpdatePositions(false);
+  };
+
+  const updateSizesAndCalculatePositions = () => {
+    calculatePositions(events);
+  };
+
+  useLayoutEffect(() => {
+    function updateSize() {
+      console.log(windowSizes);
+      updateSizesAndCalculatePositions();
+    }
+    window.addEventListener("resize", updateSize);
+    updateSize();
+    return () => window.removeEventListener("resize", updateSize);
+  }, [windowSizes]);
+
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        position: "relative",
+      }}
+    >
+      <div style={{ position: "absolute", top: 0 }}>8h</div>
+      {eventWithpositions
+        ? eventWithpositions.map((event) => (
+            <Event
+              key={event.id}
+              event={event}
+              windowSizes={windowSizes}
+              position={event.position}
+            />
+          ))
+        : "pas de données"}
+      <div style={{ position: "absolute", bottom: 0 }}>21h</div>
+    </div>
+  );
+}
+
+function useWindowSize() {
+  const [size, setSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize({ width: window.innerWidth, height: window.innerHeight });
+    }
+    window.addEventListener("resize", updateSize);
+    updateSize();
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
+  console.log(size);
+  return size;
 }
 
 export default App;
